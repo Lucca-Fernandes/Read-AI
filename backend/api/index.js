edbdfe7dc52f479c6f5803586f55be9efeed40b5
Lucca Fernandes de Castro <160192989+Lucca-Fernandes@users.noreply.github.com>
2026-02-03 10:36:47 -0300

    Ensure SSL connection settings for Neon DB

--- backend/api/index.js
+++ backend/api/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false // Obrigatório para Neon DB
+        rejectUnauthorized: false 
     }
 });
 
